d5ea3bec57c8ec3f1469c197a626d587ff2160bc
Drew Olson <drew@drewolson.org>
2025-11-26 17:30:33 -0600

    Add Aoc.Tests

---
+++ src/Aoc.Tests/ParserTests.fs
@@ -0,0 +1,25 @@
+namespace Aoc.Tests
+
+module ParserTests =
+    open Xunit
+    open FsCheck.Xunit
+    open XParsec
+    open XParsec.Parsers
+    open XParsec.CharParsers
+    open System.Collections.Immutable
+
+    [<Fact>]
+    let ``sepByEndBy works with a trailing seperator`` () =
+        let result =
+            "1;2;3;" |> Aoc.Parser.parse (Aoc.Parser.sepEndBy pint32 (pstring ";") .>> eof)
+
+        Assert.Equal([| 1; 2; 3 |], result)
+
+    [<Property>]
+    let ``sepByEndBy parses arbitrary sized collections`` (list: ImmutableArray<int>) =
+        let input = list |> Seq.map (fun i -> i.ToString()) |> String.concat ";"
+
+        let result =
+            input |> Aoc.Parser.parse (Aoc.Parser.sepEndBy pint32 (pstring ";") .>> eof)
+
+        list = result
