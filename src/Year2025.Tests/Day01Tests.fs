1ee0c6f209ff2643e67c74c666f475d47726a853
Drew Olson <drew@drewolson.org>
2025-11-30 23:25:20 -0600

    2025 day 01

--- src/Year2025.Tests/Day01Tests.fs
+++ src/Year2025.Tests/Day01Tests.fs
@@ -4,8 +4,21 @@ module ``day 01`` =
     open Xunit
     open Year2025
 
-    [<Fact>]
-    let ``part 1`` () = Assert.Equal(1, Day01.part1 "foo")
+    let input =
+        """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"""
 
     [<Fact>]
-    let ``part 2`` () = Assert.Equal(2, Day01.part2 "foo")
+    let ``part 1`` () = Assert.Equal(3, Day01.part1 input)
+
+    [<Fact>]
+    let ``part 2`` () = Assert.Equal(6, Day01.part2 input)
--- src/Year2025/Day01.fs
+++ src/Year2025/Day01.fs
@@ -1,6 +1,54 @@
 namespace Year2025
 
 module Day01 =
-    let part1 _input = 1
+    open Aoc
+    open XParsec
+    open XParsec.CharParsers
 
-    let part2 _input = 2
+    type Rotation =
+        | Left of int
+        | Right of int
+
+    let pLeft = pchar 'L' >>. pint32 |>> Left
+
+    let pRight = pchar 'R' >>. pint32 |>> Right
+
+    let pRotations = Parser.sepEndBy1 (pLeft <|> pRight) newline
+
+    let rec shrink n =
+        if n > 99 then shrink (n - 100)
+        elif n < 0 then shrink (n + 100)
+        else n
+
+    let expand r =
+        match r with
+        | Left i -> Seq.replicate i -1
+        | Right i -> Seq.replicate i 1
+
+    let part1 =
+        Parser.parse pRotations
+        >> Seq.mapFold
+            (fun s r ->
+                let n =
+                    match r with
+                    | Left i -> s - i
+                    | Right i -> s + i
+                    |> shrink
+
+                n, n)
+            50
+        >> fst
+        >> Seq.filter (fun l -> l = 0)
+        >> Seq.length
+
+    let part2 =
+        Parser.parse pRotations
+        >> Seq.collect expand
+        >> Seq.fold
+            (fun (c, s) n ->
+                let x = c + n |> shrink
+                if x = 0 then x, s + 1 else x, s
+
+            )
+            (50, 0)
+        >> snd
